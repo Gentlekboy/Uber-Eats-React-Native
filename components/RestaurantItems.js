e854fa0f93c0c42de71f25c48d1b10b511402502
gentlekboy <gentlekboy@gmail.com>
2022-05-24 07:54:09 +0100

    designed restaurant items component

---
+++ components/RestaurantItems.js
@@ -0,0 +1,93 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export const localRestaurants = [
+  {
+    name: "Beachside Bar",
+    image_url:
+      "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?cs=srgb&dl=pexels-chan-walrus-941861.jpg&fm=jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$75",
+    reviews: 1244,
+    rating: 4.5,
+  },
+
+  {
+    name: "Benihana",
+    image_url:
+      "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?cs=srgb&dl=pexels-chan-walrus-941861.jpg&fm=jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$45",
+    reviews: 756,
+    rating: 3.8,
+  },
+
+  {
+    name: "India's Grill",
+    image_url:
+      "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?cs=srgb&dl=pexels-chan-walrus-941861.jpg&fm=jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$50",
+    reviews: 213,
+    rating: 4.9,
+  },
+];
+
+export default function RestaurantItems(props) {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 20 }}>
+      {props.restaurantData.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
+}
+
+const RestaurantImage = (props) => (
+  <>
+    <Image
+      source={{
+        uri: props.image,
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", top: 20, right: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="white" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestaurantInfo = (props) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
+      <Text style={{ fontSize: 13, color: "grey" }}>30-45 • min</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 15,
+      }}
+    >
+      <Text>{props.rating}</Text>
+    </View>
+  </View>
+);
