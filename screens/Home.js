f97074a7ccd5dca94476cb45392e9cd308164a6e
gentlekboy <gentlekboy@gmail.com>
2022-06-01 16:25:08 +0100

    fetched data from yelp

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTab from "../components/HeaderTab";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
@@ -12,6 +12,28 @@ const YELP_API_KEY =
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiego";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => {
+        res.json();
+      })
+      .then((json) => setRestaurantData(json.businesses))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
 
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
